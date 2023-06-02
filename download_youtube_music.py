efe9eca0eb6b28a0f4d0e22928a677071a694098
Tiago Macedo Cardoso <contato.cardosotiago@gmail.com>
2023-06-02 01:47:20 -0300

    Refatoração do código em alguns pontos; Adição da opção para download de playlists; Adição da conversão real do arquivo ao invés de somente renomear.

--- download_youtube_music.py
+++ download_youtube_music.py
@@ -1,63 +1,120 @@
 from pytube import YouTube
+from pytube import Playlist
 import os
 from tkinter import *
+from moviepy.editor import *
 
-def call_music_download(e):
-    music_download()
+def call_download():
+    link_input = str(link.get())
 
-def music_download():
-
-    link_music = str(link.get())
-
-    if link_music == "Sair" or link_music == "sair" or link_music == "SAIR" or link_music == "exit" or link_music == "Exit" or link_music == "EXIT":
+    if link_input == "Sair" or link_input == "sair" or link_input == "SAIR" or link_input == "exit" or link_input == "Exit" or link_input == "EXIT":
         window.destroy()
 
-    else:
-        try:
-            youtube_var = YouTube(link_music)
-            
-            out_path = youtube_var.streams.filter(only_audio=True, file_extension='mp4').first().download()
-            new_path = os.path.splitext(out_path)
-            os.rename(out_path, new_path[0]+'.mp3')
-            
-            text_end["text"] = "Música: '" + youtube_var.title + "' baixada"
-            text_end.config(bg='green')
+    selected = var_select.get()
 
+    if selected == "m":
+        music_download(link_input)
+    elif selected == "p":
+        playlist_download(link_input)
+    else:
+        text_end["text"] = "Selecione uma opção entre música ou playlist."
+        text_end.config(bg='red')
+
+def playlist_download(link_input):
+    link_playlist = Playlist(link_input)
+
+    index = 0
+
+    for url_music in link_playlist.video_urls:
+        music_download(url_music)
+        index+=1
+
+    if link_playlist.length == index:
+        text_end["text"] = "Playlist: " + link_playlist.title + " baixada completamente!"
+    elif link_playlist.length != index:
+        text_end["text"] = "Provavelmente alguma música não foi baixada."
+    else:
+        text_end["text"] = "Ouve um erro, verifique os campos e tente novamente!"
+
+def music_download(link_input):
+
+    link_music = link_input
+
+    try:
+        youtube_var = YouTube(link_music)
+        
+        old_path = youtube_var.streams.filter(only_audio=True, abr="128kbps", progressive=False, type="audio").get_audio_only().download()
+        old_path_split = os.path.splitext(old_path)
+        new_path = old_path_split[0]+'.mp3'
+        
+        convert_mp4_to_mp3(old_path, new_path)
+
+        text_end["text"] = "Música: '" + youtube_var.title + "' baixada"
+        text_end.config(bg='green')
+
+        link.delete(0, END)
+
+    except:
+        if link_music == "":
+            text_end["text"] = "Insira uma URL válida"
+            text_end.config(bg='#691212')
+        else:
+            text_end["text"] = "Ocorreu um erro, verifique os campos e tente novamente!"
+            text_end.config(bg='red')
             link.delete(0, END)
 
-        except:
-            if link_music == "":
-                text_end["text"] = "Insira uma URL válida"
-                text_end.config(bg='#691212')
-            else:
-                text_end["text"] = "Ocorreu um erro, tente novamente!"
-                text_end.config(bg='red')
-                link.delete(0, END)
+def convert_mp4_to_mp3(path_before, path_after):
+    file_to_convert = AudioFileClip(path_before)
+    file_to_convert.write_audiofile(path_after)
+    file_to_convert.close()
+    
+    os.remove(path_before)
 
-def destroy(e):
+def define_music(e):
+    var_select.set("m")
+
+def define_playlist(e):
+    var_select.set("p")
+
+def call_download_by_bind(e):
+    call_download()
+
+def call_destroy_by_bind(e):
     window.destroy()
 
 window = Tk()
 window.title("Baixar músicas do YouTube")
-window.maxsize(width=675, height=175)
-window.minsize(width=600, height=125)
+window.maxsize(height=180)
+window.minsize(width=630, height=180)
 
 title_window = Label(window, text="Converter do Youtube para MP3", font=("Arial", 15), bg='#9d9d9d')
 title_window.grid(column=0, row=0, pady=10, padx=10)
 
-button = Button(window, text="Clique para baixar", command=music_download, font=("Arial", 15), bg='white')
+button = Button(window, text="Clique para baixar", command=call_download, font=("Arial", 15), bg='white')
 button.grid(column=1, row=0, pady=10, padx=10)
 
-window.bind('<Return>', call_music_download)
+var_select = StringVar()
+var_select.set("m")
+
+select_music = Radiobutton(window, text="Música", font=("Arial", 12), variable=var_select, value="m")
+select_music.grid(column=0, row=1)
+
+select_playlist = Radiobutton(window, text="Playlist", font=("Arial", 12), variable=var_select, value="p")
+select_playlist.grid(column=1, row=1)
 
 title_link = Label(window, text="Insira o link:", font=("Arial", 15))
-title_link.grid(column=0, row=1, padx=10, sticky="w")
+title_link.grid(column=0, row=2, padx=10, sticky="w")
 
 link = Entry(window, font=("Arial", 15), width=55)
-link.grid(column=0, row=2, columnspan=2, padx=10)
+link.grid(column=0, row=3, columnspan=2, padx=10)
+link.focus_force()
 
-text_end = Label(window, font=("Arial", 15))
+text_end = Label(window, font=("Arial", 13))
 text_end.grid(column=0, row=4, columnspan=2, pady=10)
 
-window.bind('<Escape>', destroy)
+window.bind('<m>', define_music)
+window.bind('<p>', define_playlist)
+window.bind('<Return>', call_download_by_bind)
+window.bind('<Escape>', call_destroy_by_bind)
+
 window.mainloop()
